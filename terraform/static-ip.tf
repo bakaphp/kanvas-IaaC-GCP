b66219ac8941e3726075a3ef56b65a96057de2c6
Rafael White <rwhite@mctekk.com>
2023-03-07 14:12:32 -0400

    create a global ip instead of regional

--- terraform/static-ip.tf
+++ terraform/static-ip.tf
@@ -1,4 +1,6 @@
-resource "google_compute_address" "default" {
-  name   = "${var.cluster_name}-${terraform.workspace}-static-ip-address"
-  region = local.location
+resource "google_compute_global_address" "default" {
+  project      = var.project_id
+  name         = "${var.cluster_name}-${terraform.workspace}-static-ip-address"
+  address_type = "EXTERNAL"
+  ip_version   = "IPV6"
 }
